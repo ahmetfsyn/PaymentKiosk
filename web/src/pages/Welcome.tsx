dcb7808ee1d997147fd4147057b1aab4df780a5c
Ahmet Furkan Sayan <21010310065@ogrenci.bartin.edu.tr>
2025-09-21 15:07:28 +0300

    hotfix/onClick method name is changed

--- web/src/pages/Welcome.tsx
+++ web/src/pages/Welcome.tsx
@@ -2,13 +2,13 @@ import { useNavigate } from "react-router";
 
 const Welcome = () => {
   const navigate = useNavigate();
-  const onClickScreen = () => {
+  const handleClickScreen = () => {
     navigate("/customer/scan-product");
   };
   return (
     <div
       className="flex  flex-col items-center justify-between py-20 h-full cursor-pointer "
-      onClick={onClickScreen}
+      onClick={handleClickScreen}
     >
       <div className="bg-white/30 p-8 rounded-md backdrop-blur-md ">
         <p className="lg:text-5xl md:text-3xl font-bold text-[#f2f4f6]  ">
